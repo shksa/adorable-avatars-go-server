564e3e75f5e79c2a9180f4f0e8775b9bdd80f34c
sreekar <sreekar339@gmail.com>
2018-12-08 09:07:46 +0530

    added routesa nd handlers

--- server.go
+++ server.go
@@ -4,10 +4,25 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	adorable "github.com/ipsn/go-adorable"
 )
 
 func getRandomAvatar(w http.ResponseWriter, r *http.Request) {
-	// avatar := adorable.Random()
+	avatar := adorable.Random()
+	w.Write(avatar)
+}
+
+func getRandomAvatarBasedOnUniqueUserID(w http.ResponseWriter, r *http.Request) {
+	qParam := r.URL.Query()
+	userID := qParam.Get("userID")
+	if len(userID) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "userID is not given as a query parameter.")
+	} else {
+		avatar := adorable.PseudoRandom([]byte(userID))
+		w.Write(avatar)
+	}
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +31,7 @@ func greet(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", greet)
+	http.HandleFunc("/getRandomAvatar", getRandomAvatar)
+	http.HandleFunc("/getRandomAvatarBasedOnUniqueUserID", getRandomAvatarBasedOnUniqueUserID)
 	http.ListenAndServe(":8080", nil)
 }
